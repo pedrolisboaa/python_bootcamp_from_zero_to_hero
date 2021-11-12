00c4f46fce17b4c53f0220addbc9742213cec564
Pedro Lisboa <phlisboa2000@gmail.com>
2021-11-12 09:34:30 -0300

    Subindo!

---
+++ 01 - Object and Data Stucture Basics/strings.py
@@ -0,0 +1,28 @@
+string_1 = 'Hello'
+string_2 = "Hi"
+string_3 = "Hi, I'm fine!"
+
+print(string_1)
+print(string_2)
+print(string_3)
+
+print('----------')
+print(string_1[0])
+print(string_1[-1])
+
+# slice [start:stop:step]
+my_string = 'hello world'
+print(my_string[0])
+print(my_string[7])
+print(my_string[8])
+print(my_string[-1])
+
+# slice [start:stop:step]
+my_string = 'abcdefgijk'
+print(my_string[2::])
+print(my_string[:3])
+print(my_string[3:6])
+
+
+print('Hello World'[-3])
+print('tinker'[1:4])
---
+++ 01 - Object and Data Stucture Basics/viable_assignments.py
@@ -0,0 +1,22 @@
+a = 5
+print(a)
+
+a = 10
+print(a)
+
+print(a + a)
+
+a = a + a
+print(a)
+
+a = a + a
+print(a)
+
+print(type(a))
+a = 40.1
+print(type(a))
+
+my_income = 1000
+my_rate = 0.1
+my_taxes = my_income * my_rate
+print(my_taxes)
